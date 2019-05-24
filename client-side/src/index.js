ab94005f6ebf95da2b750c3ac8e64c80121cd481
kmatsumoto <kmatsumoto@folio-sec.com>
2019-05-24 13:27:39 +0900

    client

---
+++ client-side/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import loadable from "@loadable/component";
+
+const Hello = loadable(() => import("./Hello"));
+const Dynamic = loadable(props => import(`./${props.name}`), {
+  cacheKey: props => props.name
+});
+
+const Moment = loadable.lib(() => import("moment"));
+
+function App() {
+  const [name, setName] = useState(null);
+  console.log(name);
+
+  return (
+    <div>
+      <p>Click A or B, compoennt will be loaded dynamically!!</p>
+      <button type="button" onClick={() => setName("A")}>
+        Go to A
+      </button>
+      <button type="button" onClick={() => setName("B")}>
+        Go to B
+      </button>
+      {name && <Dynamic name={name} />}
+      <Hello />
+      <Moment>{({ default: moment }) => moment().format("HH:mm")}</Moment>
+    </div>
+  );
+}
+
+const root = document.createElement("div");
+document.body.append(root);
+
+render(<App />, root);
